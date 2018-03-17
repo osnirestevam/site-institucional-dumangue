3b684f2d1f5f0a3caf0da15d234c0f137a17dfbc
Osnir Estevam <osnirestevam@yahoo.com.br>
2018-03-17 10:08:11 -0300

    Projeto Finalizado

---
+++ php/contact/mail-content.php
@@ -0,0 +1,22 @@
+<?php
+
+	/* Coloque aqui seu e-mail MENSAGEM */
+	/*
+		Você pode editar o e-mail enviado a você notificando sobre assinantes neste arquivo. 
+Abaixo está um exemplo simples e-mail.
+
+		The $name, $email, $url, $subject and $message variables all corresponds to the form fields name attribute.
+	*/
+
+?>
+
+Olá, <br><br>
+O formulário de contato do site do Estúdio Dumangue foi utilizado.<br>
+Aqui estão os dados da mensagem:<br>
+Nome: <?php echo $name ?><br>
+E-mail: <?php echo $email ?><br>
+URL: <?php echo $url ?><br>
+Assunto: <?php echo $subject ?><br>
+Mensagem: <?php echo $message ?>
+<br><br>
+Obrigado
